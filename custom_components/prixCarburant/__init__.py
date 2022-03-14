7a1ae951f37b05af4c647fa69f359669e115d053
Nicolas Dupuis <nicolas86200@gmail.com>
2022-03-14 21:15:15 +0100

    changement de client Ajout longitude latitude

--- custom_components/prixCarburant/__init__.py
+++ custom_components/prixCarburant/__init__.py
@@ -1,1 +1,1 @@
-REQUIREMENTS  = ['Prix-Carburant-FR-Client==1.0.3']
+REQUIREMENTS = ['Prix-Carburant-FR-Gloird-Client==0.0.1']
--- custom_components/prixCarburant/sensor.py
+++ custom_components/prixCarburant/sensor.py
@@ -24,6 +24,8 @@ ATTR_E85_LAST_UPDATE = 'last_update_e85'
 ATTR_ADDRESS = "station_address"
 ATTR_NAME = "station_name"
 ATTR_LAST_UPDATE = "last_update"
+ATTR_LONGITUDE = "longitude"
+ATTR_LATITUDE = "latitude"
 
 CONF_MAX_KM = 'maxDistance'
 CONF_SCAN_INTERVAL = 'scanInterval'
@@ -43,7 +45,8 @@ PLATFORM_SCHEMA = PLATFORM_SCHEMA.extend({
 
 
 def setup_platform(hass, config, add_devices, discovery_info=None):
-    from prixCarburantClient.prixCarburantClient import PrixCarburantClient
+    from prixCarburantGloirdClient.prixCarburantGloirdClient import \
+        PrixCarburantGloirdClient
     logging.basicConfig(stream=sys.stderr, level=logging.DEBUG)
     logging.info("[prixCarburantLoad] start")
     """Configurer la plate-forme de capteurs."""
@@ -65,7 +68,7 @@ def setup_platform(hass, config, add_devices, discovery_info=None):
         'lng': str(longitude)
     }]
 
-    client = PrixCarburantClient(homeLocation, maxDistance)
+    client = PrixCarburantGloirdClient(homeLocation, maxDistance)
     client.load()
 
     if not listToExtract:
@@ -84,9 +87,326 @@ def setup_platform(hass, config, add_devices, discovery_info=None):
     logging.info("[prixCarburantLoad] " +
                  str(len(stations)) + " stations trouvées")
     client.clean()
+    add_devices([UpdatePrix(client)])
     for station in stations:
         add_devices(
-            [PrixCarburant(stations.get(station), client, "mdi:currency-eur")])
+            [PrixCarburant(stations.get(station), client, "mdi:currency-eur"), PrixGazoil(stations.get(station), client, "mdi:currency-eur"), PrixE10(stations.get(station), client, "mdi:currency-eur"), PrixE95(stations.get(station), client, "mdi:currency-eur")])
+
+
+class UpdatePrix(Entity):
+    """Représentation d'un capteur."""
+
+    def __init__(self, client):
+        """Initialiser le capteur."""
+
+        logging.info("[updatePrix][UPDATE] Création du sensor")
+        self._state = None
+        self.client = client
+        self._icon = "mdi:refresh-auto"
+        self.lastUpdate = self.client.lastUpdate
+        self._unique_id = "UpdatePrix"
+
+    @property
+    def name(self):
+        """Renvoie le nom du capteur."""
+        return "UpdatePrix"
+
+    @property
+    def state(self):
+        """Renvoyer l'état du capteur."""
+        return self._state
+
+    @property
+    def unit_of_measurement(self):
+        """Renvoyer l'unité de mesure."""
+        return "date_time"
+
+    @property
+    def unique_id(self) -> str:
+        """Renvoie l'identifiant unique de ce capteur."""
+        return 123
+
+    @property
+    def icon(self) -> str:
+        """Renvoie l'icône mdi de l'entité."""
+        return self._icon
+
+    @property
+    def extra_state_attributes(self):
+        """Renvoyer les attributs d'état de l'appareil de la dernière mise à jour."""
+
+        attrs = {}
+        return attrs
+
+    def update(self):
+        """Récupérer de nouvelles données d'état pour le capteur.
+
+         C'est la seule méthode qui devrait récupérer de nouvelles données pour Home Assistant.
+        """
+
+        self.client.load()
+        logging.info("[updatePrix][UPDATE] mise a jour des prix")
+
+        if self.lastUpdate != self.client.lastUpdate:
+            logging.info(
+                "[updatePrix][UPDATE] les données on changer - " + str(self.lastUpdate))
+
+        self.lastUpdate = self.client.lastUpdate
+
+        self._state = self.lastUpdate
+        self.client.clean()
+
+
+class PrixGazoil(Entity):
+    """Représentation d'un capteur."""
+
+    def __init__(self, station, client, icon):
+        """Initialiser le capteur."""
+
+        logging.info("[prixCarburantLoad][UPDATE][" +
+                     station.id+"_gazoil] Création du sensor")
+        self._state = None
+        self.station = station
+        self.client = client
+        self._icon = icon
+        self._state = self.station.gazoil['valeur']
+        self.lastUpdate = self.client.lastUpdate
+        self.lastUpdateTech = self.client.lastUpdate
+        self._unique_id = "PrixCarburant_" + self.station.id+"_gazoil"
+
+    @property
+    def name(self):
+        """Renvoie le nom du capteur."""
+        return "PrixCarburant_" + self.station.id+"_gazoil"
+
+    @property
+    def state(self):
+        """Renvoyer l'état du capteur."""
+        return self._state
+
+    @property
+    def unit_of_measurement(self):
+        """Renvoyer l'unité de mesure."""
+        return "€"
+
+    @property
+    def unique_id(self) -> str:
+        """Renvoie l'identifiant unique de ce capteur."""
+        return f"{self._unique_id}"
+
+    @property
+    def icon(self) -> str:
+        """Renvoie l'icône mdi de l'entité."""
+        return self._icon
+
+    @property
+    def extra_state_attributes(self):
+        """Renvoyer les attributs d'état de l'appareil de la dernière mise à jour."""
+
+        attrs = {
+            ATTR_ID: self.station.id,
+            ATTR_GASOIL: self.station.gazoil['valeur'],
+            ATTR_GASOIL_LAST_UPDATE: self.station.gazoil['maj'],
+            ATTR_ADDRESS: self.station.adress,
+            ATTR_NAME: self.station.name,
+            ATTR_LONGITUDE: self.station.longitude,
+            ATTR_LATITUDE: self.station.latitude,
+            ATTR_LAST_UPDATE: self.client.lastUpdate.strftime(
+                '%Y-%m-%d %H:%M:%S')
+        }
+        logging.info("[prixCarburantLoad][UPDATE]["+self.station.id +
+                     "_gazoil] Mise a jour des attrs - "+str(attrs))
+        return attrs
+
+    def update(self):
+        """Récupérer de nouvelles données d'état pour le capteur.
+
+         C'est la seule méthode qui devrait récupérer de nouvelles données pour Home Assistant.
+        """
+
+        self.client.reloadIfNecessary()
+        logging.info("[prixCarburantLoad][UPDATE][" +
+                     self.station.id+"_gazoil] mise a jour du sensor")
+        list = []
+        list.append(str(self.station.id))
+        myStation = self.client.extractSpecificStation(list)
+        self.station = myStation.get(self.station.id)
+        if self.lastUpdate != self.client.lastUpdate:
+            logging.info("[prixCarburantLoad][UPDATE]["+self.station.id +
+                         "_gazoil] les données on changer - " + str(self.lastUpdate))
+
+        self.lastUpdate = self.client.lastUpdate
+
+        self._state = self.station.gazoil['valeur']
+        self.client.clean()
+
+
+class PrixE95(Entity):
+    """Représentation d'un capteur."""
+
+    def __init__(self, station, client, icon):
+        """Initialiser le capteur."""
+
+        logging.info("[prixCarburantLoad][UPDATE][" +
+                     station.id+"_e95] Création du sensor")
+        self._state = None
+        self.station = station
+        self.client = client
+        self._icon = icon
+        self._state = self.station.e95['valeur']
+        self.lastUpdate = self.client.lastUpdate
+        self.lastUpdateTech = self.client.lastUpdate
+        self._unique_id = "PrixCarburant_" + self.station.id+"_e95"
+
+    @property
+    def name(self):
+        """Renvoie le nom du capteur."""
+        return "PrixCarburant_" + self.station.id+"_e95"
+
+    @property
+    def state(self):
+        """Renvoyer l'état du capteur."""
+        return self._state
+
+    @property
+    def unit_of_measurement(self):
+        """Renvoyer l'unité de mesure."""
+        return "€"
+
+    @property
+    def unique_id(self) -> str:
+        """Renvoie l'identifiant unique de ce capteur."""
+        return f"{self._unique_id}"
+
+    @property
+    def icon(self) -> str:
+        """Renvoie l'icône mdi de l'entité."""
+        return self._icon
+
+    @property
+    def extra_state_attributes(self):
+        """Renvoyer les attributs d'état de l'appareil de la dernière mise à jour."""
+
+        attrs = {
+            ATTR_ID: self.station.id,
+            ATTR_E95: self.station.e95['valeur'],
+            ATTR_E95_LAST_UPDATE: self.station.e95['maj'],
+            ATTR_ADDRESS: self.station.adress,
+            ATTR_NAME: self.station.name,
+            ATTR_LONGITUDE: self.station.longitude,
+            ATTR_LATITUDE: self.station.latitude,
+            ATTR_LAST_UPDATE: self.client.lastUpdate.strftime(
+                '%Y-%m-%d %H:%M:%S')
+        }
+        logging.info("[prixCarburantLoad][UPDATE]["+self.station.id +
+                     "_e95] Mise a jour des attrs - "+str(attrs))
+        return attrs
+
+    def update(self):
+        """Récupérer de nouvelles données d'état pour le capteur.
+
+         C'est la seule méthode qui devrait récupérer de nouvelles données pour Home Assistant.
+        """
+
+        self.client.reloadIfNecessary()
+        logging.info("[prixCarburantLoad][UPDATE][" +
+                     self.station.id+"_e95] mise a jour du sensor")
+        list = []
+        list.append(str(self.station.id))
+        myStation = self.client.extractSpecificStation(list)
+        self.station = myStation.get(self.station.id)
+        if self.lastUpdate != self.client.lastUpdate:
+            logging.info("[prixCarburantLoad][UPDATE]["+self.station.id +
+                         "_e95] les données on changer - " + str(self.lastUpdate))
+
+        self.lastUpdate = self.client.lastUpdate
+
+        self._state = self.station.e95['valeur']
+        self.client.clean()
+
+
+class PrixE10(Entity):
+    """Représentation d'un capteur."""
+
+    def __init__(self, station, client, icon):
+        """Initialiser le capteur."""
+
+        logging.info("[prixCarburantLoad][UPDATE][" +
+                     station.id+"_e10] Création du sensor")
+        self._state = None
+        self.station = station
+        self.client = client
+        self._icon = icon
+        self._state = self.station.e10['valeur']
+        self.lastUpdate = self.client.lastUpdate
+        self.lastUpdateTech = self.client.lastUpdate
+        self._unique_id = "PrixCarburant_" + self.station.id+"_e10"
+
+    @property
+    def name(self):
+        """Renvoie le nom du capteur."""
+        return "PrixCarburant_" + self.station.id+"_e10"
+
+    @property
+    def state(self):
+        """Renvoyer l'état du capteur."""
+        return self._state
+
+    @property
+    def unit_of_measurement(self):
+        """Renvoyer l'unité de mesure."""
+        return "€"
+
+    @property
+    def unique_id(self) -> str:
+        """Renvoie l'identifiant unique de ce capteur."""
+        return f"{self._unique_id}"
+
+    @property
+    def icon(self) -> str:
+        """Renvoie l'icône mdi de l'entité."""
+        return self._icon
+
+    @property
+    def extra_state_attributes(self):
+        """Renvoyer les attributs d'état de l'appareil de la dernière mise à jour."""
+
+        attrs = {
+            ATTR_ID: self.station.id,
+            ATTR_E10: self.station.e10['valeur'],
+            ATTR_E10_LAST_UPDATE: self.station.e10['maj'],
+            ATTR_ADDRESS: self.station.adress,
+            ATTR_NAME: self.station.name,
+            ATTR_LONGITUDE: self.station.longitude,
+            ATTR_LATITUDE: self.station.latitude,
+            ATTR_LAST_UPDATE: self.client.lastUpdate.strftime(
+                '%Y-%m-%d %H:%M:%S')
+        }
+        logging.info("[prixCarburantLoad][UPDATE]["+self.station.id +
+                     "_e10] Mise a jour des attrs - "+str(attrs))
+        return attrs
+
+    def update(self):
+        """Récupérer de nouvelles données d'état pour le capteur.
+
+         C'est la seule méthode qui devrait récupérer de nouvelles données pour Home Assistant.
+        """
+
+        self.client.reloadIfNecessary()
+        logging.info("[prixCarburantLoad][UPDATE][" +
+                     self.station.id+"_e10] mise a jour du sensor")
+        list = []
+        list.append(str(self.station.id))
+        myStation = self.client.extractSpecificStation(list)
+        self.station = myStation.get(self.station.id)
+        if self.lastUpdate != self.client.lastUpdate:
+            logging.info("[prixCarburantLoad][UPDATE]["+self.station.id +
+                         "_e10] les données on changer - " + str(self.lastUpdate))
+
+        self.lastUpdate = self.client.lastUpdate
+
+        self._state = self.station.e10['valeur']
+        self.client.clean()
 
 
 class PrixCarburant(Entity):
@@ -151,6 +471,8 @@ class PrixCarburant(Entity):
             ATTR_GPL_LAST_UPDATE: self.station.gpl['maj'],
             ATTR_ADDRESS: self.station.adress,
             ATTR_NAME: self.station.name,
+            ATTR_LONGITUDE: self.station.longitude,
+            ATTR_LATITUDE: self.station.latitude,
             ATTR_LAST_UPDATE: self.client.lastUpdate.strftime(
                 '%Y-%m-%d %H:%M:%S')
         }
